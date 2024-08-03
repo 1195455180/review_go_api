481219d6b5430bc950a517cf0c986fa441a962ef
zhangxiaolei <m13161731771@163.com>
2024-08-03 14:49:26 +0800

    配置方案的实现

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+// Initialize 处罚加载 config 包的所有 init 函数
+func Initialize() {
+
+}
--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	fmt.Println("\r\n main_env  " + env)
+
 	// 初始化 Gin 实例
 	router := gin.New()
 
 	bootstrap.SetRoute(router)
 
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 
 	if err != nil {
 		// 错误处理
