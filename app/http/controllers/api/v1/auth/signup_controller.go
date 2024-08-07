b566f28892311be37c0b4cea92305996fcd3b076
zhangxiaolei <m13161731771@163.com>
2024-08-07 14:50:04 +0800

    Email 是否已注册接口

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -16,12 +16,7 @@ type SignupController struct {
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-
-	request := PhoneExistRequest{}
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析 JSON 请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -50,5 +45,40 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	// 检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exit": user.IsphoneExist(request.Phone),
+	})
+}
+
+// IsEmailExist 检测邮箱是否已注册
+func (sc *SignupController) IsEmailExist(c *gin.Context) {
+
+	request := requests.SignupEmailExistRequest{}
+
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败,返回 422 状态吗和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+
+		// 打印错误数据
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	// 表单验证
+	errs := requests.ValidateSignupEmailExist(&request, c)
+	// errs 返回长度等于零通过
+	if len(errs) > 0 {
+		// 验证失败
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	// 检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exit": user.IsEmailExist(request.Email),
 	})
 }
