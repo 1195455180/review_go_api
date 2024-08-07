8c818789252f47fef8661d10fda2f44547087572
zhangxiaolei <m13161731771@163.com>
2024-08-07 14:01:13 +0800

    手机是否注册接口

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"fmt"
 	v1 "gohub/app/http/controllers/api/v1"
+	"gohub/app/models/user"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,4 +19,24 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	type PhoneExistRequest struct {
 		Phone string `json:"phone"`
 	}
+
+	request := PhoneExistRequest{}
+
+	// 解析 JSON 请求
+	if err := c.ShouldBindJSON(&request); err != nil {
+		// 解析失败,返回 422 状态吗和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err.Error(),
+		})
+
+		// 打印错误数据
+		fmt.Println(err.Error())
+		// 出错了，中断请求
+		return
+	}
+
+	// 检查数据库并返回响应
+	c.JSON(http.StatusOK, gin.H{
+		"exit": user.IsphoneExist(request.Phone),
+	})
 }
--- routes/api.go
+++ routes/api.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,12 +11,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 	// 测试一个 v1 的路由组 所有 v1 版本的路由都放到这里
 	v1 := r.Group("/v1")
 	{
-		// 注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			// 以 Json 格式相应
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "World",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
